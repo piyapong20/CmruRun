cc7b07b830ec7467e94aef517db777d0695adf26
Piyapong <ijikonaru@hotmail.com>
2016-07-01 11:29:09 +0700

    Score Finish

--- app/src/main/java/devded/silkyland/cmrurun/ExerciseActivity.java
+++ app/src/main/java/devded/silkyland/cmrurun/ExerciseActivity.java
@@ -30,7 +30,7 @@ public class ExerciseActivity extends AppCompatActivity {
             choice3RadioButton, choice4RadioButton;
     private String[] myQuestionStrings, myChoice1Strings, myChoice2Strings,
             myChoice3Strings, myChoice4Strings, myAnswerStrings;
-    private int timesAnInt = 0;
+    private int timesAnInt = 0, scoreAnInt = 0, userChooseAnInt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +68,27 @@ public class ExerciseActivity extends AppCompatActivity {
 
         SynQuestion synQuestion = new SynQuestion();
         synQuestion.execute();
+
+        //Get Value From RadioGroup
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                switch (i) {
+                    case R.id.radioButton6:
+                        userChooseAnInt = 1;
+                    case R.id.radioButton7:
+                        userChooseAnInt = 2;
+                    case R.id.radioButton8:
+                        userChooseAnInt = 3;
+                    case R.id.radioButton9:
+                        userChooseAnInt = 4;
+                        break;
+
+                }
+
+            }   // onCheck
+        });
 
     } // Main Method
 
@@ -177,13 +198,19 @@ public class ExerciseActivity extends AppCompatActivity {
 
         } else if (timesAnInt < 5) {
 
-            if (timesAnInt !=4) {
+            //Check Score
+            if (userChooseAnInt == Integer.parseInt(myAnswerStrings[timesAnInt])) {
+                scoreAnInt += 1;
+                Log.d("1JulyV4", "Score ==> " + scoreAnInt);
+            }
+
+            if (timesAnInt != 4) {
                 timesAnInt += 1;
             }
 
             radioGroup.clearCheck();
 
-            questionTextView.setText(Integer.toString(timesAnInt + 1 ) +
+            questionTextView.setText(Integer.toString(timesAnInt + 1) +
                     ". " + myQuestionStrings[timesAnInt]);
             choice1RadioButton.setText(myChoice1Strings[timesAnInt]);
             choice2RadioButton.setText(myChoice2Strings[timesAnInt]);
