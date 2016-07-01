4ee397c4bf547116c2ef467d5d52b48892a747ed
Piyapong <ijikonaru@hotmail.com>
2016-07-01 10:12:21 +0700

    Read on JSON

--- app/src/main/java/devded/silkyland/cmrurun/ExerciseActivity.java
+++ app/src/main/java/devded/silkyland/cmrurun/ExerciseActivity.java
@@ -1,12 +1,21 @@
 package devded.silkyland.cmrurun;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ExerciseActivity extends AppCompatActivity {
 
@@ -52,7 +61,72 @@ public class ExerciseActivity extends AppCompatActivity {
         nameTextView.setText(nameString);
         stationTextView.setText("ฐานที่ " + Integer.toString(Integer.parseInt(goldString) + 1));
 
+        SynQuestion synQuestion = new SynQuestion();
+        synQuestion.execute();
+
     } // Main Method
+
+    private class SynQuestion extends AsyncTask<Void, Void, String> {
+
+        // Explicit
+        private static final String urlJSON = "http://swiftcodingthai.com/cmru/get_question.php";
+
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJSON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                Log.d("1JulyV1", "e doIn ==> " + e.toString());
+                return null;
+            }
+
+        }   // doIn
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("1JulyV1", "JSON ==> " + s);
+
+            try {
+                JSONArray jsonArray = new JSONArray(s);
+
+                String[] questionStrings = new String[jsonArray.length()];
+                String[] choice1Strings = new String[jsonArray.length()];
+                String[] choice2Strings = new String[jsonArray.length()];
+                String[] choice3Strings = new String[jsonArray.length()];
+                String[] choice4Strings = new String[jsonArray.length()];
+                String[] answerStrings = new String[jsonArray.length()];
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    questionStrings[i] = jsonObject.getString("Question");
+                    choice1Strings[i] = jsonObject.getString("Choice1");
+                    choice2Strings[i] = jsonObject.getString("Choice2");
+                    choice3Strings[i] = jsonObject.getString("Choice3");
+                    choice3Strings[i] = jsonObject.getString("Choice4");
+                    answerStrings[i] = jsonObject.getString("Answer");
+
+                    Log.d("1JulyV2", "question(" + i + ") = " + questionStrings[i]);
+
+                }   // For
+
+            } catch (Exception e) {
+                Log.d("1JulyV1", "e onPost ==> " + e.toString());
+            }
+        }
+
+    }   // SynQuestion
+
 
     public void clickAnswer(View view) {
 
